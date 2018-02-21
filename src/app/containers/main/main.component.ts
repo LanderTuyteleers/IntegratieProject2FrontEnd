7f75bcca65f79cb14da4762b5bf50c5376add932
Beau Boeynaems <beau.boeynaems@student.kdg.be>
2018-02-21 11:54:52 +0100

    jwt

--- src/app/containers/main/main.component.ts
+++ src/app/containers/main/main.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpLoginServiceService} from "../../services/http-login-service.service";
 import {UserService} from "../../services/user.service";
+import {AppDataService} from "../../services/app-data.service";
+import {USERNAME} from "../../services/auth.constant";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
   private router: Router;
   private service: HttpLoginServiceService;
   session = true;
+  user$;
 
-  constructor(router: Router, service: HttpLoginServiceService, private userService: UserService) {
+  constructor(router: Router, service: HttpLoginServiceService, private userService: UserService, private appDataService: AppDataService) {
     this.router = router;
     this.service = service;
-/*    if (!this.service.loginComplete){
-      router.navigateByUrl('login');
-    }*/
+  }
+
+  ngOnInit() {
+    this.user$ = this.appDataService.getUser(USERNAME);
   }
 
   sessionClick(){
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Headers} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {LoginUser} from "../model/loginUser";
-import {TOKEN_NAME} from "./auth.constant";
+import {TOKEN_NAME, USERNAME} from "./auth.constant";
 
 @Injectable()
 export class AuthService {
@@ -18,7 +18,7 @@ export class AuthService {
     headers.append('Authorization', 'Bearer ' + TOKEN_NAME);
     headers.append("Content-Type", "application/json");
 
-
+    localStorage.setItem(USERNAME, loginUser.username);
     return this.http.post(AuthService.AUTH_TOKEN, loginUser, {headers})
       .map(res => res.json())
       .map((res: any) => {
