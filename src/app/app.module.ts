71e7b9e7f7b87a251e075d0d7ac94b1479a7b713
Gino <gino.moukhi@student.kdg.be>
2018-03-17 10:50:06 +0100

    Clean slate start for game front-end pre Kendo UI drawing implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 import {SignupFormComponent} from './components/signup-form/signup-form.component';
-import {FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NavigationBarComponent} from './components/navigation-bar/navigation-bar.component';
 import {LoginFormComponent} from './components/login-form/login-form.component';
 import {FormComponent} from './components/form/form.component';
 import {HttpLoginServiceService} from './services/http-login-service.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { MainComponent } from './containers/main/main.component';
-import { LoginComponent } from './containers/login/login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MainComponent} from './containers/main/main.component';
+import {LoginComponent} from './containers/login/login.component';
 import {AppRoutingModule} from './app-routing.module';
-import { ProfileComponent } from './components/profile/profile.component';
-import { SessionComponent } from './components/session/session.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {SessionComponent} from './components/session/session.component';
 import {TOKEN_NAME} from './services/auth.constant';
 import {AuthConfig, AuthHttp} from 'angular2-jwt';
 import {Http, HttpModule} from '@angular/http';
 import {AuthService} from './services/auth.service';
 import {UserService} from './services/user.service';
-import { GameScreenComponent } from './containers/game-screen/game-screen.component';
-import { LobbyListComponent } from './components/lobby-list/lobby-list.component';
-import { ChatboxComponent } from './components/chatbox/chatbox.component';
-import { GameComponent } from './components/game/game.component';
-import { LobbyItemUserComponent } from './components/lobby-item-user/lobby-item-user.component';
-import { GameLaneComponent } from './components/game-lane/game-lane.component';
-import { CanvasComponent } from './components/canvas/canvas.component';
+import {GameScreenComponent} from './containers/game-screen/game-screen.component';
+import {LobbyListComponent} from './components/lobby-list/lobby-list.component';
+import {ChatboxComponent} from './components/chatbox/chatbox.component';
+import {GameComponent} from './components/game/game.component';
+import {LobbyItemUserComponent} from './components/lobby-item-user/lobby-item-user.component';
 
 export function authHttpServiceFactory(http: Http) {
   return new AuthHttp(new AuthConfig({
@@ -52,9 +50,7 @@ export function authHttpServiceFactory(http: Http) {
     LobbyListComponent,
     ChatboxComponent,
     GameComponent,
-    LobbyItemUserComponent,
-    GameLaneComponent,
-    CanvasComponent
+    LobbyItemUserComponent
   ],
   imports: [
     BrowserModule,
@@ -62,7 +58,7 @@ export function authHttpServiceFactory(http: Http) {
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
   ],
   providers: [
     {provide: AuthHttp, useFactory: authHttpServiceFactory, deps: [Http]},
@@ -72,4 +68,5 @@ export function authHttpServiceFactory(http: Http) {
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Card} from '../../model/Card';
 
 @Component({
   selector: 'app-game',
@@ -7,20 +6,11 @@ import {Card} from '../../model/Card';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  jup: Card;
-  stella: Card;
-  maes: Card;
-  beers: Array<Card>;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.jup = new Card('Jupiler', 'https://jupiler.be/media/img/campaigns/facepainter/allinred-logo.png');
-    this.stella = new Card('Stella Artois', 'https://seeklogo.com/images/S/Stella_Artois-logo-2B093ACD7A-seeklogo.com.png');
-    this.maes = new Card('Maes', 'http://bier.blog.nl/files/2012/05/logo-maes.png');
-
-    this.beers = [this.jup, this.stella, this.maes];
   }
 
 }
